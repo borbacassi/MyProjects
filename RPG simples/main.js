ae2eaa8f3c56d9883c36e47bc4daeaa6d1191ee0
Cassiano Borba <bcassiano888@gmail.com>
2026-03-01 20:36:31 -0300

    Update main.js
    adicionado classes e inimigos, faltam adicionar mensageiros de dano, dano aleatório e metodos adicionais para cada classe

--- RPG simples/main.js
+++ RPG simples/main.js
@@ -10,15 +10,80 @@ class Character {
         return dano
     }
     taVivo(){
-        return (this.hp > 0) ? "Sim":"Não";
+        return this.hp > 0;
+    }
+
+    ataca(alvo){
+        const dano = this.attack;
+        alvo.takeDamage(dano);
+        return dano;
     }
 }
 
 class Hero extends Character{
-    constructor(name){
-        super(name,100,20,5);
+    constructor(name, hp = 100, attack = 10, defense = 10){
+        super(name,hp,attack,defense);
         this.level = 1
     }
 } 
-let p1 = new Hero("Art")
-console.log(p1)
+class Enemy extends Character{
+    constructor(name, hp = 100, attack = 10 , defense = 10){
+        super(name,hp,attack,defense);
+        this.level = 1;
+    }
+}
+
+class Goblin extends Enemy{
+    constructor(name = "Goblin"){
+        super(name,40,15,10);
+    }
+}
+
+class Dragon extends Enemy{
+    constructor(name = "Dragão"){
+        super(name,150,30,15);
+    }
+    ataca(alvo){
+        const dano = alvo.takeDamage(this.attack*2)
+        console.log(`${this.name} atacou com baforada! Dano: ${dano}.`)
+        return dano;
+    }
+}
+
+class Mago extends Hero{
+    constructor(name){
+    super(name,80,35,10);
+    }
+    ataca(alvo){
+        const dano = alvo.takeDamage(this.attack+5);
+        console.log(`${this.name} atacou com bola de fogo; Dano causado: ${dano}`)
+        return dano;
+    }
+}
+
+class Batalha{
+    constructor(heroi,inimigo){
+        this.heroi = heroi;
+        this.inimigo = inimigo;
+        this.log = []
+    }
+    turno(){
+        while (this.heroi.taVivo() && this.inimigo.taVivo()) {
+           const dmg = this.heroi.ataca(this.inimigo);
+           this.log.push(`${this.heroi.name} causou ${dmg} de dano`)
+            if(!this.inimigo.taVivo()){ 
+                return `${this.inimigo.name} Morreu! Fim do turno`
+            }
+            const edmg = this.inimigo.ataca(this.heroi);
+            this.log.push(`${this.inimigo.name} causou ${edmg} de dano`)
+            if(!this.heroi.taVivo()){
+                return `${this.heroi.name} Morreu! Fim do turno`
+            }
+        }
+    }
+}
+let h1 = new Mago("Art")
+let e1 = new Goblin()
+let b1 = new Batalha(h1,e1)
+console.log(b1.turno())
+console.log(b1.log)
